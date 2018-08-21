762996662943ad5fdeb06fc5849ec873ede17ae2
Evgeny Bondarenko <evgeny.bondarenko@onetwotrip.com>
2018-08-21 17:57:40 +0300

    seems like working!

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const moment = require('moment');
 const config = require('config');
 const debug = require('debug')('devDuty:server');
 const Promise = require('bluebird');
+const Slack = require('slack');
 
 
 // Load client secrets from a local file.
 const credentials = require('./config/credentials.json');
 const token = require('./config/token.json');
 
+const slackBot = new Slack({token: config.token});
 
 debug.enabled = true;
 
@@ -41,6 +43,7 @@ function usertimeTableHash(timetable) {
 
 const timeTableCache = {};
 const userCache = {};
+const slackUserCache = {};
 
 async function updateTimeTables() {
   const year = moment().format('Y');
@@ -51,7 +54,7 @@ async function updateTimeTables() {
     .filter((el, index, arr) => {
       return arr.findIndex(item => item.hash === el.hash) === index;
     });
-  Promise.map(uniqueTimeTables, async (timetable) => {
+  return Promise.map(uniqueTimeTables, async (timetable) => {
     const {prefix, spreadsheetId} = timetable;
     const {hash} = timetable;
     if (!timeTableCache[hash]) {
@@ -73,14 +76,12 @@ async function updateTimeTables() {
       const cols = transpose(rows);
       for (let month = 0; month < 12 && cols[month * 2] && cols[month * 2][0]; month++) {
         const realMonth = cols[month * 2][0];
-        debug(`Found month ${realMonth}`);
         const dateColumn = cols[month * 2].slice(2);
         const devColumn = cols[month * 2 + 1].slice(2);
         dateColumn.forEach((row, index) => {
           if (!row || !devColumn[index]) {
             return;
           }
-          debug(`${row}: ${devColumn[index]}`);
           timeTableCache[hash][year] = timeTableCache[hash][year] || {};
           timeTableCache[hash][year][realMonth] = timeTableCache[hash][year][realMonth] || {};
           timeTableCache[hash][year][realMonth][row] = devColumn[index];
@@ -101,7 +102,7 @@ async function updateUsers() {
     .filter((el, index, arr) => {
       return arr.findIndex(item => item.hash === el.hash) === index;
     });
-  Promise.map(uniqueTimeTables, async (timetable) => {
+  return Promise.map(uniqueTimeTables, async (timetable) => {
     const {spreadsheetId} = timetable;
     const {hash} = timetable;
     if (!userCache[hash]) {
@@ -134,5 +135,123 @@ async function updateUsers() {
   }, {concurrency: 2});
 }
 
-updateTimeTables();
-updateUsers();
+async function updateSlackUsers() {
+  const users = await slackBot.users.list();
+  if (!users.ok) {
+    debug(`updateSlackUsers error, smth not okay: ${users}`);
+    return false;
+  }
+  users.members.forEach((user) => {
+    slackUserCache[user.name] = user.id;
+  });
+  debug(`SlackUserCache: ${JSON.stringify(slackUserCache, null, 3)}`);
+  return true;
+}
+
+async function updateSlackUserName(options) {
+  const {group, devIndex, devName} = options;
+  const channelData = await slackBot.groups.info({channel: group});
+  if (!channelData.ok) {
+    debug(`updateSlackUserName error, smth not okay: ${channelData}`);
+    return false;
+  }
+  const topic = channelData.group.topic.value;
+  debug(`Current topic: ${topic}`);
+  const findUsers = /<@[A-Z0-9]+>/g;
+  const foundUsers = topic.match(findUsers);
+  debug(`Found users: ${foundUsers}`);
+  if (!foundUsers) {
+    debug('users not found in topic!');
+    return false;
+  }
+  if (!foundUsers[devIndex]) {
+    debug(`users with index ${devIndex} not found in topic!`);
+    return false;
+  }
+  const devId = slackUserCache[devName];
+  if (!devId) {
+    debug(`Developer ${devName} not found in cache!`);
+    return false;
+  }
+  const newTopic = topic.replace(foundUsers[devIndex], `<@${devId}>`);
+  if (newTopic === topic) {
+    debug('current dev already set, nothing to do');
+    return true;
+  }
+  debug(`Setting topic ${newTopic}`);
+  const response = await slackBot.groups.setTopic({
+    channel: group,
+    topic: newTopic,
+  });
+  return response && response.ok === true;
+}
+
+async function updateSlack() {
+  const year = moment().format('Y');
+  const month = moment().format('M');
+  const day = moment().format('D');
+  return Promise.map((config.timetables), async (timetable) => {
+    const calendar = timeTableCache[timeTableHash(timetable)];
+    const users = userCache[usertimeTableHash(timetable)];
+    if (!calendar) {
+      debug(`No calendar data for timetable ${JSON.stringify(timetable, null, 3)}`);
+      return false;
+    }
+    if (!users) {
+      debug(`No calendar data for timetable ${JSON.stringify(timetable, null, 3)}`);
+      return false;
+    }
+    const updateTime = moment(moment().format(`YYYY-MM-DD ${timetable.updateTime}`), 'YYYY-MM-DD HH:mm:ss');
+    debug(`Update time: ${updateTime.format('YYYY-MM-DD HH:mm:ss')}`);
+    const {lastUpdate} = timetable;
+    if (lastUpdate) {
+      debug(`lastUpdate: ${lastUpdate.format('YYYY-MM-DD HH:mm:ss')}`);
+    }
+    else {
+      debug('lastUpdate: none');
+    }
+    if (!lastUpdate || moment().isAfter(updateTime) && updateTime.isAfter(lastUpdate)) {
+      debug('Need update!');
+      try {
+        const currentDevName = calendar[year][month][day];
+        const currentDevSlackName = users[currentDevName];
+        const options = {
+          group: timetable.group,
+          devIndex: timetable.devIndex || 0,
+          devName: currentDevSlackName,
+        };
+        const res = await updateSlackUserName(options);
+        if (res) {
+          timetable.lastUpdate = moment();
+          return true;
+        }
+        return false;
+      }
+      catch (err) {
+        debug(`Failed to find current dev: ${err}`);
+      }
+      debug('Updated.');
+    }
+    debug('No need for update.');
+    return false;
+  }, {concurrency: 2});
+}
+
+const updateInterval = 1000 * 60 * 30;// every half an hour
+// const updateInterval = 1000 * 20;// every 60 second
+async function run() {
+  return Promise.all([updateTimeTables(), updateUsers(), updateSlackUsers()])
+    .then(() => {
+      return updateSlack();
+    })
+    .catch((err) => {
+      debug(`ERR: ${err}`);
+    });
+}
+
+async function start() {
+  await run();
+  setInterval(() => run(), updateInterval);
+}
+
+start();
