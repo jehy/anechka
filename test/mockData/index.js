9b29d26da85a4f08fd859c038aa55a1666dbf07f
Evgeny Bondarenko <evgeny.bondarenko@onetwotrip.com>
2019-05-14 15:00:33 +0300

    add some tests

---
+++ test/mockData/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const spreadsheets = require('./spreadSheets/index');
+
+module.exports = {
+  spreadsheets,
+};
---
+++ test/mockData/spreadSheets/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const timetable = require('./timetable');
+const users = require('./users');
+
+module.exports = {
+  users,
+  timetable,
+};
